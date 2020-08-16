f9c7db4d8fe4ae6430a4ef22f087b8175b59d6e6
EkkoYu <17712427671@163.com>
2020-08-16 15:05:52 +0800

    第1次:创建GitDemo.java文件

---
+++ GitDemo.java
@@ -0,0 +1,18 @@
+package cn.itcast.git.demo;
+
+/**
+ * Git示例
+ */
+public class GitDemo {
+  
+  
+  public void add () {
+  
+     System.out.println("Hello Git");
+     System.out.println("2.比较差异");
+     System.out.println("3.测试版本回退");
+     System.out.println("4.测试撤销、还原");
+     
+  }
+  
+}
